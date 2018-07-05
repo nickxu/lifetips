a784a69ead36cc353172becd74e0be93aee229f0
xubaoxiang <xbxmsn@msn.cn>
2018-07-05 16:39:26 +0800

    optimize home page style

--- js/index.js
+++ js/index.js
@@ -1,44 +1,52 @@
 var lifeTips = [];
 eachData = {
-    firstText: "好思想，好生活。",
-    text_time: "Created at 2017-08-26 13:00"
+    textTitle: "好思想，好生活。",
+    textTime: "2017-08-26 13:00",
+    textContent: "好思想，好生活。"
 };
 lifeTips.push(eachData);
 eachData = {
-    firstText: "剪不断，理还乱。",
-    text_time: "Created at 2017-08-28 09:56"
+    textTitle: "剪不断，理还乱。",
+    textTime: "2017-08-28 09:56",
+    textContent: "剪不断，理还乱。"
 };
 lifeTips.push(eachData);
 eachData = {
-    firstText: "清官难断家务事。",
-    text_time: "Created at 2017-08-28 09:57"
+    textTitle: "清官难断家务事。",
+    textTime: "2017-08-28 09:57",
+    textContent: "清官难断家务事。"
 };
 lifeTips.push(eachData);
 eachData = {
-    firstText: "对于人生的成功来说，工具是辅助因素，而不是决定因素。这些工具如电脑，手机等。",
-    text_time: "Created at 2017-08-30 12:06"
+    textTitle: "工具就是工具",
+    textTime: "2017-08-30 12:06",
+    textContent: "对于人生的成功来说，工具是辅助因素，而不是决定因素。这些工具如电脑，手机等。"
 };
 lifeTips.push(eachData);
 eachData = {
-    firstText: "定期解决高优先级技术债务问题可以实现产品的持续健康度。",
-    text_time: "Created at 2017-08-31 12:39"
+    textTitle: "解决技术债务",
+    textTime: "2017-08-31 12:39",
+    textContent: "定期解决高优先级技术债务问题可以实现产品的持续健康度。"
 };
 lifeTips.push(eachData);
 eachData = {
-    firstText: "一曲《笑傲江湖》",
-    text_time: "Created at 2017-09-05 18:27"
+    textTitle: "一曲《笑傲江湖》",
+    textTime: "2017-09-05 18:27",
+    textContent: "一曲《笑傲江湖》"
 };
 lifeTips.push(eachData);
 eachData = {
-    firstText: "看过鲁迅的文章，觉得这个人说的还是比较实在的，还是有些道理的。",
-    text_time: "Created at 2017-09-05 18:28"
+    textTitle: "评鲁迅",
+    textTime: "2017-09-05 18:28",
+    textContent: "看过鲁迅的文章，觉得这个人说的还是比较实在的，还是有些道理的。"
 };
 lifeTips.push(eachData);
 eachData = {
-    firstText: "忙碌起来吧，小伙伴们！",
-    text_time: "Created at 2017-09-19 17:36"
+    textTitle: "业精于勤",
+    textTime: "2017-09-19 17:36",
+    textContent: "忙碌起来吧，小伙伴们！"
 };
 lifeTips.push(eachData);
 $(function () {
-    showListByJson("text_block", lifeTips, "tips_template");
+    showListByJson("content_block", lifeTips, "content_template");
 });
